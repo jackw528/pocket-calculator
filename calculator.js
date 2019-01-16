e27da8b95dc3c00b669d69eb7a47dde79e36b378
Jack Weiss <jack.weiss528@gmail.com>
2019-01-16 13:21:12 -0500

    worked on clear button

--- calculator.js
+++ calculator.js
@@ -1,6 +1,3 @@
 function input(num){
-   document.calc.display.value = document.calc.display.value + num;
-}
-function clear(){
-  document.calc.display.value="";
+  document.calc.display.value = document.calc.display.value + num;
 }
